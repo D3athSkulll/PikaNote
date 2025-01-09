7eeb44db3df08612e014de0d744e8c1406179163
D3athSkulll <shivam.deolankar@gmail.com>
2025-01-10 02:27:39 +0530

    feature: added event detection of keypress

--- src/editor.rs
+++ src/editor.rs
@@ -1,35 +1,34 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
-pub struct Editor {
-    
-}
+pub struct Editor {}
 
-impl Editor{
-    pub fn default()-> Self{
-        Editor{}
+impl Editor {
+    pub fn default() -> Self {
+        Editor {}
     }
 
+    pub fn run(&self) {
+        enable_raw_mode();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
 
-pub fn run(&self){
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes(){
-        match b{
-            Ok(b)=>{
-                let c = b as char;
-                if c.is_control(){
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                }else{
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} '{1}'\r", b, c);
+                        _ => (),
+                    }
                 }
-                if c == 'q'{
-                    break;
-                }
+
+                Err(err) => println!("Error: {}", err),
+                _ => (),
             }
-            Err(err)=> println!("Error: {}",err),
         }
+        disable_raw_mode().unwrap();
     }
-    disable_raw_mode().unwrap();
 }
-}
-
