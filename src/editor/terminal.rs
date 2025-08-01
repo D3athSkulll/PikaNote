3995660a56a1fff21e2b6db952478cda23edc692
D3athSkulll <shivam.deolankar@gmail.com>
2025-08-01 18:36:38 +0530

    bugfix: Fixed some errors in code.

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,92 +1,70 @@
-
-use crossterm::cursor::MoveTo;
-use crossterm::cursor::Hide;
-use crossterm::cursor::Show;
-
-use crossterm::{queue,Command};
-use crossterm::terminal::size;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::style::Print;
-use std::io::{stdout,Write,Error};
-
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use crossterm::{queue, Command};
+use std::io::{stdout, Error, Write};
 use core::fmt::Display;
 
-
-#[derive(Copy,Clone)]
-pub struct Size{
-    pub width: u16,
+#[derive(Copy, Clone)]
+pub struct Size {
     pub height: u16,
+    pub width: u16,
 }
-pub struct Position{
+#[derive(Copy, Clone)]
+pub struct Position {
     pub x: u16,
     pub y: u16,
 }
 pub struct Terminal;
 
-impl Terminal{
-
-    pub fn initialize()-> Result<(), Error>{
-        enable_raw_mode()?;
-        Self::clear_screen();
-        
-        
-        Self::move_cursor_to(Position{x:0,y:0})?;
-        Ok(())
-    }
-
-    pub fn terminate()-> Result<(), Error>{
+impl Terminal {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
         disable_raw_mode()?;
         Ok(())
-        
     }
-
-    
-
-    pub fn clear_screen()-> Result<(), Error>{
-        
-        Self::queue!(stdout(), Clear(ClearType::All))?;
-        Self::queue!(stdout(), Clear(ClearType::Purge))?;   
-        
+    pub fn initialize() -> Result<(), Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
-
-    pub fn clear_current_line()-> Result<(), Error>{
-        Self::queue!(stdout(), Clear(ClearType::CurrentLine))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
-
-    pub fn hide_cursor()-> Result<(), Error>{
-        Self::queue!(stdout(), Hide)?;
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-    pub fn show_cursor()-> Result<(), Error>{
-        Self::queue!(stdout(), Show)?;
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        Self::queue_command(MoveTo(position.x, position.y))?;
         Ok(())
     }
-
-    pub fn move_cursor_to(position: Position) ->Result<(), Error>{
-        Self::queue!(stdout(), MoveTo(position.x,position.y))?;
+    pub fn hide_cursor() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
         Ok(())
     }
-
-    pub fn print(string: &str) -> Result<(),Error>
-    {
-        Self::queue!(stdout(), Print(string))?;
+    pub fn show_cursor() -> Result<(), Error> {
+        Self::queue_command(Show)?;
         Ok(())
     }
-
-    pub fn size() -> Result<Size, Error>{
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+    pub fn size() -> Result<Size, Error> {
         let (width, height) = size()?;
-        Ok(Size{width, height})
+        Ok(Size { height, width })
     }
-
-    pub fn execute() -> Result<(), Error>{
+    pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
         Ok(())
     }
 
-    fn queue_command<T:Command>(command : T)-> Result<(),Error>{
-        queue!(stdout(),command)?;
+    fn queue_command<T:Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 }
